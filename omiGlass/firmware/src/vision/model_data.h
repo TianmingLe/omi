104f0db1a3f29f4b6dc0c7111305f9430c2f9125
TianmingLe <1767976037@qq.com>
2026-04-26 09:12:41 +0000

    chore(vision): add model_data contract and placeholder model

---
+++ omiGlass/firmware/src/vision/model_data.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdint.h>
+
+extern const unsigned char g_feature_model[];
+extern const unsigned int g_feature_model_len;
+
+extern const float g_feature_quant_scale;
+extern const int8_t g_feature_quant_zero_point;
+extern const uint8_t g_feature_model_id;
+
